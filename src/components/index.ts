ebc01b50417203d09a9397b047c856cbd8c2dfbf
nnnozimjon <iamnozimjon@gmail.com>
2024-05-29 10:30:46 +0500

    feat: some changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,13 @@
 import dynamic from "next/dynamic";
 
-export const Logo = dynamic(() => import("./logo"), { ssr: true });
-export const Header = dynamic(() => import("./Header"), { ssr: true });
-export const Footer = dynamic(() => import("./Footer"), { ssr: true });
-export const SearchBar = dynamic(() => import("./SearchBar"), { ssr: true });
+export const Logo = dynamic(() => import("./logo"), { ssr: false });
+export const Header = dynamic(() => import("./Header"), { ssr: false });
+export const Footer = dynamic(() => import("./Footer"), { ssr: false });
+export const SearchBar = dynamic(() => import("./SearchBar"), { ssr: false });
 export const ProductCard = dynamic(() => import("./ProductCard"), {
-  ssr: true,
+  ssr: false,
 });
 
 export const EmptyPlaceholder = dynamic(() => import("./EmptyPlaceholder"), {
-  ssr: true,
+  ssr: false,
 });
